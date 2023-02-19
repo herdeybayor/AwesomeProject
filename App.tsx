f5765a2956c9b4ad72df78a7a593be882162d2c8
herdeybayor <shereefdeen4real@gmail.com>
2023-02-19 08:25:24 +0100

    feat: add readme

--- App.tsx
+++ App.tsx
@@ -1,14 +1,40 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
+import * as Linking from "expo-linking";
+
 import AddNewPaletteModal from "./screens/AddNewPaletteModal";
 import MainStackScreen from "./screens/MainStackScreen";
 import { RootStackParamList } from "./typings";
 
 const App = () => {
   const RootStack = createNativeStackNavigator<RootStackParamList>();
+  const linking: LinkingOptions<RootStackParamList> = {
+    prefixes: [Linking.createURL("/")],
+    config: {
+      screens: {
+        Main: {
+          screens: {
+            Home: {
+              screens: {
+                Home: "home",
+              },
+            },
+            ColorPalette: {
+              screens: {
+                ColorPalette: "palette/:paletteName",
+              },
+            },
+          },
+        },
+        AddNewPalette: "add-new-palette",
+      },
+    },
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <RootStack.Navigator>
         <RootStack.Screen
           name="Main"
---
+++ README.md
@@ -0,0 +1,5 @@
+#Awesome Project
+- git clone https://github.com/herdeybayor/AwesomeProject.git
+- cd AwesomeProject
+- yarn install
+- yarn start
